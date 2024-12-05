7b1c26db8f883dbd05ce7fb8539156a8b71ad0ed
XenoMC <vinscurse@gmail.com>
2024-12-05 13:41:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Listening to Servers", "Playing Discord"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["Listening to Servers", "Listening to servers"];
+const statusTypes = [ 'online', 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
