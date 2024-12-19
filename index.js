3d96b3633122b925ebfef3f1a2fc569f79263127
vinswish <Makhi52612@gmail.com>
2024-12-19 10:34:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Listening to Servers", "Listening to servers"];
+const statusMessages = ["Listening to Servers", "Listening to Servers"];
 const statusTypes = [ 'online', 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
