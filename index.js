5baedece90b06b2b53836f443eb7109bdffabba9
Xeno <Makhi52612@gmail.com>
2025-01-27 15:17:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Listening To Servers", "Listening To Servers"];
+const statusMessages = ["Listening To Servers", "Listening To Ego."];
 const statusTypes = [ 'online', 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
