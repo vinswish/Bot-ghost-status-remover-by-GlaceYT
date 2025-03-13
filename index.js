29c6692881c2d9a466c4840544e697d9371f899d
XenoMC <vinscurse@gmail.com>
2025-03-13 00:26:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Listening To Servers", "Listening To Ego"];
+const statusMessages = ["Watching Servers", "Watching Servers"];
 const statusTypes = [ 'online', 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
